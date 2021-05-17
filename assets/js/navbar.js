6aa73dfb684836b195735ff42a82fc212a3a62bf
Elias Hawa <58529029+EliasJRH@users.noreply.github.com>
2021-05-16 22:11:04 -0400

    Fix navbar (#17)
    * Have image pull from absolute url
    
    * Made navbar hidden under normal cirumstances
    Implemented proper fadein/fadeout
    
    * Add navbar.js script to header.html
    Removed script tag from navbar.html
    
    * Add script back to navbar.html
    
    * add thing i shouldt've removed
    
    * move nav js script
    
    * remove staic scripts
    
    * Removed unnecessary parameters from keepContent
    
    * Fixed item blocks fading away even on hover
    
    * added style modifications (#20)
    
    * added style modifications
    
    * Reworked js to work with responsive header
    
    * Run prettier
    
    * Removed unneeded parameters
    
    * formatted
    
    * fixed block centering style
    
    Co-authored-by: Elias Hawa <elias.hawa2468@gmail.com>
    
    Co-authored-by: MathyouMB <matthewmacraebovell@bell.net>
    Co-authored-by: Matthew M-B <matthewmacraebovell@outlook.com>

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,5 +1,100 @@
-function loadNavbar() {
-  console.log("Loaded Navbar");
+globalTimeout = null;
+
+//When hovered over calls this function
+function showContent(container, blocks, lefts, rights) {
+  // console.log(`Showing content ${container}`)
+  hideUnneededContent(container, blocks, lefts, rights, true);
 }
 
-window.addEventListener("load", loadNavbar());
+//hides everything that shouldn't be seen
+function hideUnneededContent(container, blocks, lefts, rights, hoverOver) {
+  // console.log(`Hiding content on ${hoverOver ? 'hover over' : 'hover out'}`)
+  accContents = document.getElementsByClassName("actual_content");
+  for (var i = 0; i < accContents.length; i++) {
+    if (accContents[i].id !== container) {
+      accContents[i].style.display = "none";
+    }
+  }
+  hoverOver
+    ? setTimeout(function () {
+        changeDisplayToFlex(container, blocks, lefts, rights);
+      }, 10)
+    : (globalTimeout = setTimeout(function () {
+        makeContentTransparent(container, blocks, lefts, rights);
+      }, 10));
+}
+
+//changes the display of actual_content from none to flex
+function changeDisplayToFlex(container, blocks, lefts, rights) {
+  // console.log(`Changing display of ${container} to flex`)
+  document.getElementById(container).style.display = "flex";
+  document.getElementById(container).style.position = "relative";
+  growItemBoxes(container, blocks, lefts, rights);
+}
+
+//Changes the height of any item boxes inside actual_content
+function growItemBoxes(container, blocks, lefts, rights) {
+  // console.log(`Growing item boxes ${blocks}`)
+  left = document.getElementsByClassName(lefts);
+  right = document.getElementsByClassName(rights);
+  block = document.getElementsByClassName(blocks);
+  for (var i = 0; i < block.length; i++) {
+    block[i].style.animation = "grow 400ms forwards";
+    left[i].style.animation = "grow 400ms forwards";
+    right[i].style.animation = "grow 400ms forwards";
+  }
+  globalTimeout = setTimeout(function () {
+    makeContentOpaque(container);
+  }, 100);
+}
+
+//makes everything in actual_content opaque
+function makeContentOpaque(container) {
+  // console.log(`Making ${container} opaque`)
+  document.getElementById(container).style.animation = "fadein 600ms forwards";
+}
+
+//This function is called on hover out
+function hideContent(container, blocks, lefts, rights) {
+  // console.log(`Hiding content ${container}`)
+  hideUnneededContent(container, blocks, lefts, rights, false);
+}
+
+//Makes hovered out content transparent
+function makeContentTransparent(container, blocks, lefts, rights) {
+  // console.log(`Making ${container} transparent`)
+  document.getElementById(container).style.animation = "fadeout 300ms forwards";
+  globalTimeout = setTimeout(function () {
+    shrinkItemBoxes(container, blocks, lefts, rights);
+  }, 100);
+}
+
+//shrinks hovered out content
+function shrinkItemBoxes(container, blocks, lefts, rights) {
+  // console.log(`Shrinking item boxes ${blocks}`)
+  left = document.getElementsByClassName(lefts);
+  right = document.getElementsByClassName(rights);
+  block = document.getElementsByClassName(blocks);
+  for (var i = 0; i < block.length; i++) {
+    block[i].style.animation = "shrink 400ms forwards";
+    left[i].style.animation = "shrink 400ms forwards";
+    right[i].style.animation = "shrink 400ms forwards";
+  }
+  globalTimeout = setTimeout(function () {
+    changeDisplayToNone(container);
+  }, 300);
+}
+
+//changes display of hovered out content to none
+function changeDisplayToNone(container) {
+  // console.log(`Changing display of ${container} to none`)
+  document.getElementById(container).style.display = "none";
+  document.getElementById(container).style.position = "fixed";
+}
+
+function keepContent(container, blocks) {
+  clearTimeout(globalTimeout);
+  document.getElementById(container).style.animationPlayState = "paused";
+  document.getElementById(container).style.animation = "none";
+  document.getElementById(container).style.opacity = "1";
+}
