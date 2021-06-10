92ca70188eabc0a2460e483807279dbcf65cb6a6
Darkkz <jake.diedrich@gmail.com>
2021-06-10 19:03:50 -0400

    Added a description div below the slideshow

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,24 +1,28 @@
-var index = 0;
+let index = 0;
 showSlides();
 
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dots");
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dots");
+    let captions = document.getElementsByClassName("description");
 
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  index++;
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        captions[i].style.display = "none";
+    }
+    index++;
 
-  if (index > slides.length) {
-    index = 1;
-  }
+    if (index > slides.length) {
+        index = 1;
+    }
 
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[index - 1].style.display = "block";
-  dots[index - 1].className += " active";
-  setTimeout(showSlides, 5000);
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[index - 1].style.display = "block";
+    captions[index - 1].style.display = "block";
+    dots[index - 1].className += " active";
+    setTimeout(showSlides, 5000);
 }
