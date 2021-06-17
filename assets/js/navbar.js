6925c780df25bf4ed979f006438f84e3c058bbe1
Elias Hawa <elias.hawa2468@gmail.com>
2021-06-17 19:23:42 -0400

    Made date card slightly high on tablet view Misc cleanup

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -29,7 +29,7 @@ function hideUnneededContent(container, blocks, lefts, rights, hoverOver) {
 //changes the display of actual_content from none to flex
 function changeDisplayToFlex(container, blocks, lefts, rights) {
   // console.log(`Changing display of ${container} to flex`)
-  document.getElementById(container).style.pointerEvents = "auto"
+  document.getElementById(container).style.pointerEvents = "auto";
   document.getElementById(container).style.display = "flex";
   document.getElementById(container).style.position = "relative";
   growItemBoxes(container, blocks, lefts, rights);
