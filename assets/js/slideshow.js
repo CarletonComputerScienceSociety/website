d5a9304b62c5eb79766aebaa8e29fa1603aa9a2b
Elias Hawa <elias.hawa2468@gmail.com>
2021-07-05 10:51:05 -0400

    run formatter

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,29 +1,27 @@
-
-
 var urls = [
   "/images/orientation2018-min.jpeg",
   "/images/poster.jpeg",
   "/images/poster2.jpeg",
 ];
 
-var header = document.getElementsByClassName("home-header-image")
+var header = document.getElementsByClassName("home-header-image");
 
 var index = 0;
 
-function startSlideshow(){
-    index = (index + 1) % urls.length;
-    for (let i = 0; i < header.length; i++){
-        header[i].style.animation = "fadeout 500ms forwards"
-        setTimeout(function(){ changeImage(header[i], urls[index])}, 500)
-    }
-    setTimeout(startSlideshow, 2000);
+function startSlideshow() {
+  index = (index + 1) % urls.length;
+  for (let i = 0; i < header.length; i++) {
+    header[i].style.animation = "fadeout 500ms forwards";
+    setTimeout(function () {
+      changeImage(header[i], urls[index]);
+    }, 500);
+  }
+  setTimeout(startSlideshow, 2000);
 }
 
-function changeImage(header, imagePath){
-    header.style.backgroundImage = `url('/ccss-website${imagePath}')`
-    header.style.animation = "fadein 500ms forwards"
-    
+function changeImage(header, imagePath) {
+  header.style.backgroundImage = `url('/ccss-website${imagePath}')`;
+  header.style.animation = "fadein 500ms forwards";
 }
 
-startSlideshow()
-
+startSlideshow();
