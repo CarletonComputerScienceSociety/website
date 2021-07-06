4b03cf06d3b8e8658a5f29af0d16ce23f396492b
EliasJRH <EliasJRH@users.noreply.github.com>
2021-07-06 03:17:47 +0000

    deploy: 5ef09e1d53e1c39132ec9f7de188513638f5e773

--- js/bundle.cce6b794ec606da53a741abcd13a037f70fb09f2fa383a7b8f93782be04ef12d7f1bda09009749d7b1f28631155cf808dacf61b56413302d6037a20fd7c8ff1f.js
+++ js/bundle.cce6b794ec606da53a741abcd13a037f70fb09f2fa383a7b8f93782be04ef12d7f1bda09009749d7b1f28631155cf808dacf61b56413302d6037a20fd7c8ff1f.js
@@ -140,3 +140,47 @@ $(document).ready(function () {
     $(this).toggleClass("down");
   });
 });
+
+;
+var urls = ["/images/v0saWei.jpg", "/images/orientation2018-min.jpeg"];
+
+const fadeDuration = 500; //in milliseconds
+const imageTransitionDuration = 10000;
+
+var frontHeader = document.getElementById("front");
+var backHeader = document.getElementById("back");
+var frontHeaderMobile = document.getElementById("front-mobile");
+var backHeaderMobile = document.getElementById("back-mobile");
+
+var slideshowIndex = 0;
+
+function startSlideshow() {
+  slideshowIndex = (slideshowIndex + 1) % urls.length;
+  backHeader.style.backgroundImage = `url('/ccss-website${urls[slideshowIndex]}')`;
+  frontHeader.style.animation = `fadeout ${fadeDuration}ms forwards`;
+  setTimeout(function () {
+    changeImage(frontHeader, urls[slideshowIndex]);
+  }, fadeDuration);
+
+  backHeaderMobile.style.backgroundImage = `url('/ccss-website${urls[slideshowIndex]}')`;
+  frontHeaderMobile.style.animation = `fadeout ${fadeDuration}ms forwards`;
+  setTimeout(function () {
+    changeImage(frontHeaderMobile, urls[slideshowIndex]);
+  }, fadeDuration);
+
+  if (navigator.userAgent.indexOf("Chrome") != -1) {
+    frontHeader.style.animation = `fadein ${fadeDuration}ms forwards`;
+    frontHeaderMobile.style.animation = `fadein ${fadeDuration}ms forwards`;
+  }
+
+  setTimeout(startSlideshow, imageTransitionDuration); //in milliseconds how long each image is visible for
+}
+
+function changeImage(header, imagePath) {
+  header.style.backgroundImage = `url('/ccss-website${imagePath}')`;
+  if (navigator.userAgent.indexOf("Firefox") != -1) {
+    header.style.animation = `fadein ${fadeDuration}ms forwards`;
+  }
+}
+
+startSlideshow();
